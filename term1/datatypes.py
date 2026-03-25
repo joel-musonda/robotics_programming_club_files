6205e6179457fd0785720f3de1b1f55a92c6a323
joel-musonda <joel1musonda2@gmail.com>
2026-03-25 12:43:41 -0400

    adding lists

--- term1/datatypes.py
+++ term1/datatypes.py
@@ -5,9 +5,44 @@
 name = input("what is your name? ")
 print("Hello " + name)
 
-#data types
 
 #numric data types
+
+#this is an integer
 num1 = 5
 print(type(num1))
+
+#this is a float
+num2 = 5.5
+print(type(num2))
+
+#this is a complex number
+num3 = 5 + 2j
+print(type(num3))
+
+#sequence data types
+
+#this is a list
+names = ["Alice", "Bob", "Charlie"]
+print(type(names))
+
+#this is a tuple
+coordinates = (10, 20, 30, 40)
+print(type(coordinates))
+
+#this is a range
+numbers = range(1, 10)
+print(type(numbers))
+
+#boolean data types
+3 > 2 #this will return true
+3 < 2 #this will return false
+
+
+num = range(4)
+
+for i in num:
+    print(i)
+
+
 
